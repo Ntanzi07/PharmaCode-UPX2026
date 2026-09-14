851da7c3e1de6f0fb7dd3238a0b34fb27b0529bb
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2026-09-14 19:13:34 -0300

    feat: adding the new routers

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,7 +9,11 @@ import (
 func New(drugH *handler.DrugHandler) http.Handler {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /drugs", drugH.ListDrugs)
+	mux.HandleFunc("POST /drugs", drugH.CreateDrug)
 	mux.HandleFunc("GET /drugs/ean/{ean}", drugH.GetSummaryByEAN)
+	mux.HandleFunc("PUT /drugs/{id}", drugH.UpdateDrug)
+	mux.HandleFunc("DELETE /drugs/{id}", drugH.DeleteDrug)
 
 	return mux
 }
